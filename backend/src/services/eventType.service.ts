853512cb0f8fa2bb51c7cef694385d456f6d6696
Aditya Kumbhar <adityakumbhar915@gmail.com>
2026-01-04 19:36:26 +0530

    phase 1: created the eventType service for creating the unique slug

---
+++ backend/src/services/eventType.service.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import slugify from "slugify";
+
+
+const prisma = new PrismaClient()
+
+export const generateUniqueSlug = async(title: string, userId: string): Promise<string> =>{
+    let slug = slugify(title, {lower: true, strict: true})
+    let uniqueSlug = slug
+    let counter = 1
+
+    while(await prisma.user.findFirst({where: {id: userId, slug: uniqueSlug}})){
+        uniqueSlug = `${slug}-${counter}`
+        counter ++
+    }
+
+    return uniqueSlug
+}
